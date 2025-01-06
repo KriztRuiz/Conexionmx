b599f8b3d1c2dabfd7f9fc187f4d01fb8d34ef3c
KriztRuiz <xcrcx@live.com.mx>
2025-01-06 15:25:23 -0600

    maqueta para mostrar

---
+++ js/patrocinadores.js
@@ -0,0 +1,25 @@
+const patrocinadores = [
+    { src: "images/patrocinador1.png", link: "https://www.facebook.com/profile.php?id=100013567775325" },
+    { src: "images/patrocinador2.png", link: "https://www.youtube.com/watch?v=UDB1Yfsxui4" },
+    { src: "images/patrocinador3.png", link: "https://www.youtube.com/watch?v=UxbQO-21Szg" },
+    { src: "images/patrocinador4.png", link: "https://www.youtube.com/watch?v=qQOa2PPaf9A" },
+    { src: "images/patrocinador5.png", link: "https://www.youtube.com/watch?v=QX268MIAsGw" },
+    { src: "images/patrocinador6.png", link: "https://www.facebook.com/profile.php?id=100083074103372" },
+    { src: "images/patrocinador7.png", link: "https://www.facebook.com/profile.php?id=100078469028505" },
+    { src: "images/patrocinador8.png", link: "https://www.facebook.com/TICKY68" },
+  ];
+  
+  let currentIndex = 0;
+  const imgElement = document.getElementById("patrocinador-img");
+  
+  // Cambia la imagen cada 3 segundos
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % patrocinadores.length;
+    imgElement.src = patrocinadores[currentIndex].src;
+  }, 3000);
+  
+  // Abre el enlace del patrocinador al hacer clic
+  imgElement.addEventListener("click", () => {
+    window.open(patrocinadores[currentIndex].link, "_blank");
+  });
+  
