2e71183d658936d947d29b55c4ae0f96605b4b9d
KriztRuiz <xcrcx@live.com.mx>
2025-01-15 18:52:15 -0600

    probando subir el proyecto con las revistas en AWS S3

---
+++ js/revistas-patrocinadores.js
@@ -0,0 +1,25 @@
+const patrocinadores = [
+    { src: "../Galeria/patrocinador1.png", link: "https://www.facebook.com/profile.php?id=100013567775325" },
+    { src: "../Galeria/patrocinador2.png", link: "https://www.youtube.com/watch?v=UDB1Yfsxui4" },
+    { src: "../Galeria/patrocinador3.png", link: "https://www.youtube.com/watch?v=UxbQO-21Szg" },
+    { src: "../Galeria/patrocinador4.png", link: "https://www.youtube.com/watch?v=qQOa2PPaf9A" },
+    { src: "../Galeria/patrocinador5.png", link: "https://www.youtube.com/watch?v=QX268MIAsGw" },
+    { src: "../Galeria/patrocinador6.png", link: "https://www.facebook.com/profile.php?id=100083074103372" },
+    { src: "../Galeria/patrocinador7.png", link: "https://www.facebook.com/profile.php?id=100078469028505" },
+    { src: "../Galeria/patrocinador8.png", link: "https://www.facebook.com/TICKY68" },
+];
+  
+  let currentIndex = 0;
+  const imgElement = document.getElementById("patrocinador-img");
+  
+  // Cambia la imagen cada 3 segundos
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % patrocinadores.length;
+    imgElement.src = patrocinadores[currentIndex].src;
+  }, 3000);
+  
+  // Abre el enlace del patrocinador al hacer clic
+  imgElement.addEventListener("click", () => {
+    window.open(patrocinadores[currentIndex].link, "_blank");
+  });
+  
